7319e62d5456cfcece4d40105e09c44267fe18f6
Asim Ditta <asimditta15@gmail.com>
2020-11-01 10:36:34 +0000

    completed styling of index page

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -1,15 +1,10 @@
 @extends('layouts.app')
 
 @section ('header')
-    <div class="flex justify-between">
-        <div class="text-4xl mb-8">
-            <h1>Players</h1>
-        </div>
-        <div>
-            <div class="mr-2">
-                <a href="/players/"><i class="fas fa-paw mr-2"></i>Add a Player</a>
-            </div>
-        </div>
+    <div class=" mt-10 flex justify-between">
+            <h1 class="text-4xl my-auto font-bold mb-8">Players</h1>
+            <a class="my-auto text-3xl my-auto font-bold mb-5 mr-10 transition-all duration-500 ease-in-out bg-none hover:bg-blue-200  border border-blue-300 rounded-md px-2 py-2" href="/players/">Add a Player</a>
+
     </div>
 @endsection
 
@@ -18,15 +13,15 @@
         <div class="grid gap-x-6 grid-cols-4">
 
             @foreach($players as $p)
-                <div class="bg-blue-300  mb-4 px-5 py-4 border rounded border-gray-500" >
-                    <div class="flex justify-between">
-                        <a href="{{ $p -> path }}">{{ $p -> name }}</a>
-                        <img class="w-10 mr-5" src="img/footballer.png">
+                <div class="bg-blue-200  mb-5 px-5 py-4 border rounded border-black" >
+                    <div class="flex justify-between mb-5">
+                        <a class="my-auto ml-2 text-xl font-bold" href="{{ $p -> path }}">{{ $p -> name }}</a>
+                        <img class="w-15 mr-5" src="img/footballer.png">
                     </div>
 
                     <div class="flex justify-between mx-50 my-1">
                         <p class="mt-4">
-                            <a class="hover:bg-blue-400  border border-blue-300 rounded-md mr-5 px-2 py-2" href="{{ $p -> path.('/edit')}}"> Edit Player
+                            <a class="transition-all duration-500 ease-in-out bg-none hover:bg-blue-400  border border-blue-300 rounded-md mr-5 px-2 py-2" href="{{ $p -> path.('/edit')}}"> Edit Player
                             </a>
                         </p>
 
@@ -34,7 +29,7 @@
                             @method ('DELETE')
                             @csrf
                             <button  type="submit">
-                                <i class="hover:bg-blue-400  border border-blue-300 rounded-md mr-5 px-2 py-2">Delete Player</i>
+                                <a class="transition-all duration-500 ease-in-out bg-none hover:bg-blue-400  border border-blue-300 rounded-md mr-5 px-2 py-2">Delete Player</a>
                             </button>
                         </form>
                     </div>
--- resources/views/players/show.blade.php
+++ resources/views/players/show.blade.php
@@ -2,35 +2,41 @@
 
 @section('content')
     <main class="sm:container sm:mx-auto sm:mt-10">
-        <div class="md:w-1/3 md:mx-auto">
+        <div class="md:w-3/4 md:mx-auto">
+            <div class="flex justify-between">
 
-            <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
 
-                <h1 class="class=text-xl font-bold mb-2">
-                    Name: {{ $players -> name }}
-                </h1>
 
-                <p class="mt-2">
-                    Team: {{$players -> team}}
-                </p>
 
-                <p class="mt-2">
-                    Position: {{ $players -> position}}
-                </p>
+                <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md px-5">
 
-                <p class="mt-4">
-                    <a href="{{ $players -> path.('/edit')}}"> Edit Players
-                        <i class="fas fa-edit"></i>
-                    </a>
-                </p>
+                    <h1 class="text-2xl font-bold my-10">
+                        Name: {{ $players -> name }}
+                    </h1>
 
-                <form class="mt-4" method="post" action="{{ $players -> path }}">
-                    @method ('DELETE')
-                    @csrf
-                    <button type="submit">
-                        <i>Delete Players</i>
-                    </button>
-                </form>
+                    <p class="mt-2 text-lg">
+                        Team: {{$players -> team}}
+                    </p>
+
+                    <p class="mt-2">
+                        Position: {{ $players -> position}}
+                    </p>
+
+                    <p class="mt-4">
+                        <a href="{{ $players -> path.('/edit')}}"> Edit Players
+
+                        </a>
+                    </p>
+
+                    <form class="mt-4" method="post" action="{{ $players -> path }}">
+                        @method ('DELETE')
+                        @csrf
+                        <button type="submit">
+                            <i>Delete Players</i>
+                        </button>
+                    </form>
+                </div>
+                <img class="w-25 mr-5" src="/img/footballer.png">
             </div>
         </div>
     </main>
